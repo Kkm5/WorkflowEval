9b4efc01287cd192dfe170c9db3b165e9532dc41
Kkm5 <kkm5@pitt.edu>
2016-02-03 13:31:31 -0500

    MakeWorkflowMap function initial commit

---
+++ R/MakeWorkflowMap.R
@@ -0,0 +1,17 @@
+############function MakeWorkflowMap
+
+############Description: From user Protein Expression Data (or any other primary key) create a IdMap object
+### args 1: Protein Expression data 3: first gene expression data 4-?: ... following gene expression data
+###
+###
+
+
+paste(row.names(RNASEQDATA[1:dim(RNASEQDATA)[[1]]/2,]),row.names(RNASEQDATA[(dim(RNASEQDATA)[[1]]/2)+1:dim(RNASEQDATA)[[1]],]),sep=",")
+paste(row.names(RNASEQDATA[1:67,]),row.names(RNASEQDATA[68:134,]),sep=",")
+
+MakeWorkflowMap<-function(gene_expression_data){
+  rows_gene_data<-dim(gene_expression_data)[[1]]
+  paste(row.names(gene_expression_data[1:(rows_gene_data/2),]),row.names(gene_expression_data[((rows_gene_data/2)+1):rows_gene_data,]),sep=",")
+}
+
+  
