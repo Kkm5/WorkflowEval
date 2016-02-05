8df1eb07da1b583c163d83faa9ba27c074e01eac
Kkm5 <kkm5@pitt.edu>
2016-02-05 15:07:01 -0500

    Working version of mixture distribution

--- R/WorkflowCorrelation.R
+++ R/WorkflowCorrelation.R
@@ -1,5 +1,6 @@
 x<-"hello"
 require(IdMappingAnalysis)
+options(stringsAsFactors = FALSE)
 #### suggested to call: options(stringsAsFactors = FALSE) 
 
 ################RUN ONLY ONCE UNTIL CORRECTED
@@ -23,4 +24,28 @@ IdMap_rna_rppa<-IdMap(DF=idmap_rnaseqworkflow_df,name="IdMap_rna_rppa", primaryK
 
 uniquePairs_workflow <- as.UniquePairs.IdMap(IdMap_rna_rppa,secondaryIDs)
 corrData.rnaseq <- CorrData(uniquePairs_workflow,Formatted_primary_data,Formatted_secondary_data)
-corr.rnarppa.try1 <- Corr(corrData.rnaseq,method="spearman",verbose=TRUE);
+corr.rnarppa.try1 <- Corr(corrData.rnaseq,method="spearman",verbose=TRUE)
+
+bootstrap.rnarppa<-Bootstrap(corrData.rnaseq,Fisher=TRUE,verbose=TRUE)
+
+bootModel.rna.rppa <-as.data.frame(bootstrap.rnarppa)
+bootModel.rna.rppa<-bootModel.rna.rppa[c(1:96,99:134),]
+postProbs.rnarppa<- NULL
+postProbVar.rnarppa<- NULL
+
+EMtest.rnarppa<- fit2clusters.RNAseq(bootModel.rna.rppa$corr, bootModel.rna.rppa$sd^2,psi0Constraint=0, sameV=T,estimatesOnly=F,seed=Random.seed.save)
+postProbsOvarian<-as.vector(EMtestOvarian[[1]]/(1+EMtestOvarian[[1]]))  #not needed at the output is a datafrmae from fit2clusters
+postProbVarOvarian <-as.vector(EMtestOvarian[[2]])
+
+pairs <- jointUniquePairs$as.data.frame()
+
+##########################
+pairMatchTable<-pairs
+#bootModel.fulldata<-as.data.frame(bootstrap)
+bootModel <-as.data.frame(bootstrap)
+load("dotRandom.seed.rda")
+Random.seed.save <- sync.Random.seed <- .Random.seed
+
+bootMergedWithPairs = merge(data.frame(postProbs=postProbs, postProbVar=postProbVar,bootModel), pairMatchTable)
+
+
